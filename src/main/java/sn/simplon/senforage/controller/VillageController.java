4f2d624fe37d8e2379fac50aec4a4d85889b9c4a
MoussaAKHYAR <amrane93.mb@gmail.com>
2020-11-25 02:35:03 +0000

    gestion des villages

--- src/main/java/sn/simplon/senforage/controller/VillageController.java
+++ src/main/java/sn/simplon/senforage/controller/VillageController.java
@@ -5,11 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+
 
 import sn.simplon.senforage.dao.IVillage;
 import sn.simplon.senforage.entities.Village;
@@ -25,6 +24,19 @@ public class VillageController {
 		return "village/liste";
 	}
 	
+	@PostMapping(value= {"/addVillage"})
+	public String addVillage(@Validated Village village) {
+		//save the village 
+		villagedao.save(village);
+		return "redirect:/Village";
+	}
+	
+	@GetMapping(value= {"/delete"})
+	public String deleteVillage(int id) {
+		//save the village 
+		villagedao.deleteById(id);
+		return "redirect:/Village";
+	}
 	/*
 	 * @PostMapping("Village/liste") public String add()(@ModelAttribute Village
 	 * village, Model model) { model.addAttribute("village", village); return
