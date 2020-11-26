ce1ded42d1cdfd6abe6983256143c6834eca6cb4
MoussaAKHYAR <amrane93.mb@gmail.com>
2020-11-26 10:48:18 +0000

    Gestion client complet

--- src/main/java/sn/simplon/senforage/controller/ClientController.java
+++ src/main/java/sn/simplon/senforage/controller/ClientController.java
@@ -1,5 +1,7 @@
 package sn.simplon.senforage.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
@@ -9,22 +11,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import sn.simplon.senforage.dao.IClient;
+import sn.simplon.senforage.dao.IUser;
+import sn.simplon.senforage.dao.IVillage;
 import sn.simplon.senforage.entities.Client;
 
 @Controller
 public class ClientController {
 	@Autowired
 	private IClient clientdao;
+	
+	@Autowired
+	private IVillage villagedao;
+	
+	@Autowired
+	private IUser userdao;
 
 	@GetMapping("/Client")
 	public String liste(Model model) {
 		model.addAttribute("liste_clients", clientdao.findAll(Sort.by(Sort.Direction.ASC, "id")));
+		model.addAttribute("liste_villages", villagedao.findAll(Sort.by(Sort.Direction.ASC, "id")));
+
 		return "client/liste";
 	}
 
 	@PostMapping(value = { "/addClient" })
-	public String addVillage(@Validated Client client) {
+	public String addVillage(@Validated Client client, HttpServletRequest req) {
+		//client.setVillage(villagedao.findById(Integer.parseInt(req.getParameter("id_village"))).orElse(null));
 		// save the client
+		client.setUser(userdao.getOne(1));
 		clientdao.save(client);
 		return "redirect:/Client";
 	}
